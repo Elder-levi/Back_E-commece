9284e6edceed34b74da695bd6b74b113594d06e3
Elder levi <elderlevisantos@email.com>
2026-02-11 10:51:58 -0300

    Configuraçao da IMG

--- Back_E-commece/src/main/java/com/example/Back_E_commece/Controller/ControllesProd.java
+++ Back_E-commece/src/main/java/com/example/Back_E_commece/Controller/ControllesProd.java
@@ -10,12 +10,15 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.nio.file.StandardCopyOption;
+import java.rmi.server.UID;
 import java.util.List;
+import java.util.UUID;
 
 @CrossOrigin(origins = "http://localhost:5173")
 @RestController
@@ -43,19 +46,31 @@ public class ControllesProd {
 
     @PostMapping(value = "/Cad/Produto"/*, consumes = MediaType.MULTIPART_FORM_DATA_VALUE*/)
     public ResponseEntity<Produto> Cadastro(
-            @RequestPart("produto") ProdutosDTO dto
+            @RequestPart("produto") ProdutosDTO dto ,
+            @RequestPart("Imagem") MultipartFile imagens
     ) throws IOException {
+
+        String nomeArquivo = UUID.randomUUID() + "_" + imagens.getOriginalFilename();
+
+        Path pastaIMG = Paths.get("upload");
+        Files.createDirectories(pastaIMG);
+
+        Path caminhoCompleto = pastaIMG.resolve(nomeArquivo);
+        Files.write(caminhoCompleto , imagens.getBytes());
 
         System.out.println("Nome: " + dto.nome);
         System.out.println("Descricao: " + dto.descricao);
         System.out.println("Preco: " + dto.preco);
+        System.out.println("Imagen" + imagens.getOriginalFilename());
 
+        Produto Prod = new Produto();
         Produto NewPro = new Produto();
         NewPro.setNome(dto.nome);
         NewPro.setDescricao(dto.descricao);
         NewPro.setPreco(dto.preco);
+        Prod.setImg("/upload/" + nomeArquivo);
 
-        Produto salvo = ServProd.SetProdut(NewPro);
+        Produto salvo = ServProd.SetProdut(NewPro );
         return ResponseEntity.ok(salvo);
     }
 
